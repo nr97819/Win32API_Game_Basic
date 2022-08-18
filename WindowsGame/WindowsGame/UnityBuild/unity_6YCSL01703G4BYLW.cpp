cac9fc86e4410b721307f8064a7c878857db6a53
SangWoo Cho <nr97819@naver.com>
2022-08-18 16:08:24 +0900

    43 - Unity Build
    - Unity Build는 과거에는 지원을 안해줘서 직접 만들었는데, 최근에는 Unity Build 실험 버전이 나와서 property 파일
    수정해서 번거롭게 사용은 가능하도록 지원해주다가, 이제는 아예 기능을 full로 지원해줘서 편하게 사용 가능해졌다.
    (아래 방법 참고)
    
    0.1. C/C++ -> 일반 -> 다중 프로세서 컴파일 => 예(/MP) (활성화)
    0.2. C/C++ -> 미리 컴파일된 헤더 => (만들기 -> 사용 안함) (비활성화) (다중 프로세서 컴파일 옵션과 충돌되고, 경로를 못찾는다.)
    1. 프로젝트 속성 -> 구성 속성 -> 고급 -> Unity Build 켜기 (활성화)
    2. C/C++ -> Unity 빌드 -> Unity 파일 디렉터리 => $(ProjectDir)UnityBuild\ 이런 식으로 수정
    (Unity Build를 위해 최소 소스 수만큼 cpp를 include할 파일을 여러개 만드는데, 그 Unity01~04.cpp 파일이 저장될 곳)
    3. Unity 파일의 최소 소스 수 : 10개 (8개로 하면 조금 적은 느낌) -> (12개 13개 쯤 잡히는 것 같다.)
    4. 나머지는 안건드려도 됨 (최대 소스 수는 건들 필요 X -> 자동으로 최소 이상 더 많이 include 할 수 있으면, 더 해주면 좋은것이므로)
    
    (*) C:드라이브로 부터의 local 경로 중, 한글이 조금이라도 섞여 있으면 에러가 발생한다.
    (모두 영문 경로로 수정하기)
    
    - 결과 : 3초만에 수많은 cpp 파일이 모두 빌드되는 극적인 효과를 볼 수 있다.

---
+++ WindowsGame/WindowsGame/UnityBuild/unity_6YCSL01703G4BYLW.cpp
@@ -0,0 +1,36 @@
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CRes.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CResMgr.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CScene.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CSceneMgr.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CScene_Start.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CScene_Tool.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CSound.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CTexture.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CTimeMgr.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\func.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\main.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\SelectGDI.cpp"
+
---
+++ WindowsGame/WindowsGame/UnityBuild/unity_QS2NZ6CI8QMQCJ0I.cpp
@@ -0,0 +1,39 @@
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CAnimation.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CAnimator.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CCamera.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CCollider.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CCollisionMgr.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CCore.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CEventMgr.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CKeyMgr.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CMissile.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CMonster.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CObject.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CPathMgr.cpp"
+
+
+#include "C:\__ASTR__\SourceCode\Win32API_Game_Basic\WindowsGame\WindowsGame\CPlayer.cpp"
+
